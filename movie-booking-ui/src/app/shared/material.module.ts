f2207b05db354e03378170da99860a50813e3e8d
Shweta-Hi <mostlysagacious@gmail.com>
2026-03-15 21:13:47 +0100

    Admin Dashboard

---
+++ movie-booking-ui/src/app/shared/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+@NgModule({
+exports:[
+MatToolbarModule,
+MatButtonModule,
+MatCardModule,
+MatFormFieldModule,
+MatInputModule,
+MatGridListModule
+]
+})
+export class MaterialModule{}
