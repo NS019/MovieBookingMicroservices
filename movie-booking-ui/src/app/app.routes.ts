d99be7e6565d9c050eced8f450d4156dd7d7a177
Shweta-Hi <mostlysagacious@gmail.com>
2026-03-15 17:35:52 +0100

    restructuring of project

--- movie-booking-ui/src/app/app.routes.ts
+++ movie-booking-ui/src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'movies',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'movies',
+    loadComponent: () =>
+      import('./features/movies/pages/movie-list/movie-list')
+        .then(m => m.MovieList)
+  },
+
+  {
+    path: 'browse',
+    loadComponent: () =>
+      import('./features/browse/pages/browse-page/browse-page')
+        .then(m => m.BrowsePage)
+  },
+
+  {
+    path: 'booking/:showId',
+    loadComponent: () =>
+      import('./features/booking/pages/seat-selection/seat-selection')
+        .then(m => m.SeatSelection)
+  },
+
+
+  {
+    path: '**',
+    redirectTo: 'movies'
+  }
+
+];
--- movie-booking-ui/src/app/features/movies/pages/movie-list/movie-list.ts
+++ movie-booking-ui/src/app/features/movies/pages/movie-list/movie-list.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-movie-list',
   imports: [],
   templateUrl: './movie-list.html',
